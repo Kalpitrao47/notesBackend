32122f9e1f67b5b6b52abf5e4c6cebb11fcecf98
Tanmay Sawant <tanmay.s@unlocktech.co.in>
2026-05-21 18:47:35 +0530

    changes app.js  1

--- app.js
+++ app.js
@@ -1,70 +1,30 @@
-// const express = require('express')
-// const app = express()
-// const dotenv = require('dotenv')
-// dotenv.config();    
-// const mongoose = require('mongoose');
-// const DB_URI = process.env.DB_URI
-// const PORT = process.env.PORT
-// app.use(express.json())
-// const cors = require('cors')
+const express = require('express')
+const app = express()
+const dotenv = require('dotenv')
+dotenv.config();    
+const mongoose = require('mongoose');
+const DB_URI = process.env.DB_URI
+const PORT = process.env.PORT
+app.use(express.json())
+const cors = require('cors')
 
-// app.use(cors());
-// const notesRouter = require('./router/notesRouter')
+app.use(cors());
+app.options("*", cors());
+const notesRouter = require('./router/notesRouter')
 
-// app.use('/', notesRouter)
+app.use('/', notesRouter)
 
-// mongoose.connect(DB_URI).then(() => {
-//     console.log('Database connected successfully');
-// })
-// .catch((err) => {
-//     console.log(err.message)
-// })
+mongoose.connect(DB_URI).then(() => {
+    console.log('Database connected successfully');
+})
+.catch((err) => {
+    console.log(err.message)
+})
 
-// app.listen(PORT, ()=>{
-//     console.log(`server is running on ${PORT}....`)
-// })
+app.listen(PORT, ()=>{
+    console.log(`server is running on ${PORT}....`)
+})
 
 
-const express = require("express");
-const app = express();
 
-const dotenv = require("dotenv");
-dotenv.config();
-
-const mongoose = require("mongoose");
-const cors = require("cors");
-
-const notesRouter = require("./router/notesRouter");
-
-const DB_URI = process.env.DB_URI;
-const PORT = process.env.PORT || 5000;
-
-// CORS CONFIG
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173", // local frontend
-      "https://your-frontend.vercel.app", // deployed frontend
-    ],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
-
-app.use(express.json());
-
-app.use("/", notesRouter);
-
-mongoose
-  .connect(DB_URI)
-  .then(() => {
-    console.log("Database connected successfully");
-  })
-  .catch((err) => {
-    console.log(err.message);
-  });
-
-app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`);
-});
 
